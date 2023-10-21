d7f2cafb9ebe486b8ea598f92f6fe48e7bec6e86
Filip M <filipm93@gmail.com>
2023-10-22 00:00:22 +0200

    always update recipes after navigating to recipes page

--- src/pages/recipes-page/recipes-page.tsx
+++ src/pages/recipes-page/recipes-page.tsx
@@ -7,20 +7,38 @@ import {
   Typography,
   CardActions,
   Input,
-  TextField,
+  Skeleton,
 } from '@mui/material';
 import { Container } from '@mui/system';
 import { useRecipes } from '../../contexts/recipes/recipes.context';
 import SearchIcon from '@mui/icons-material/Search';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useIngredients } from '../../contexts/ingredients/ingredients.context';
 
 export default function RecipesPage() {
   const { recipes, setSearchQuery, searchQuery } = useRecipes();
+  const { ingredients } = useIngredients();
+  const { setRecipes } = useRecipes();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
   };
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const selectedIngredients = ingredients
+        .filter((i) => i.isConfirmed)
+        .map((i) => i.name);
+
+      setIsLoading(true);
+      await setRecipes(selectedIngredients);
+      setIsLoading(false);
+    };
+
+    fetchRecipes();
+  }, []);
 
   return (
     <Container sx={{ py: 8 }} maxWidth='md'>
@@ -32,35 +50,65 @@ export default function RecipesPage() {
         sx={{ mb: 4 }}
       ></Input>
       <Grid container spacing={4}>
-        {recipes?.map((recipe) => (
-          <Grid item key={recipe.id} xs={12} sm={6} md={4}>
-            <Card
-              sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-            >
-              <CardMedia
-                component='div'
+        {isLoading ? (
+          <>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation='wave'
+                variant='rectangular'
+                height={250}
+              ></Skeleton>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation='wave'
+                variant='rectangular'
+                height={250}
+              ></Skeleton>
+            </Grid>
+            <Grid item xs={12} sm={6} md={4}>
+              <Skeleton
+                animation='wave'
+                variant='rectangular'
+                height={250}
+              ></Skeleton>
+            </Grid>
+          </>
+        ) : (
+          recipes?.map((recipe) => (
+            <Grid item key={recipe.id} xs={12} sm={6} md={4}>
+              <Card
                 sx={{
-                  // 16:9
-                  pt: '56.25%',
+                  height: '100%',
+                  display: 'flex',
+                  flexDirection: 'column',
                 }}
-                image={recipe.image}
-              />
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant='h5' component='h2'>
-                  {recipe.title}
-                </Typography>
-                <Typography>
-                  {recipe.usedIngredients.map((i) => i.originalName)}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Link to={`/recipe/${recipe.id}`}>
-                  <Button>View</Button>
-                </Link>
-              </CardActions>
-            </Card>
-          </Grid>
-        ))}
+              >
+                <CardMedia
+                  component='div'
+                  sx={{
+                    // 16:9
+                    pt: '56.25%',
+                  }}
+                  image={recipe.image}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant='h5' component='h2'>
+                    {recipe.title}
+                  </Typography>
+                  <Typography>
+                    {recipe.usedIngredients.map((i) => i.originalName)}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Link to={`/recipe/${recipe.id}`}>
+                    <Button>View</Button>
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))
+        )}
       </Grid>
     </Container>
   );

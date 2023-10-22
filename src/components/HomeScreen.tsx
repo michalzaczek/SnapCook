8735881dbc5de16b4f6a0768154bca12b017a137
michalzaczek <michal.zaczek@interia.pl>
2023-10-22 14:26:59 +0200

    Add firebase useAuth hook

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Screen from './Screen';
 import { Typography } from '@mui/material';
+import SampleComponent from '../firebase/SampleComponent';
 
 const HomeScreen: React.FC = () => {
   return (
     <Screen>
       <Typography variant='h1'>Home Screen</Typography>
+      <SampleComponent />
     </Screen>
   );
 };

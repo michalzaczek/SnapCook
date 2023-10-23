79ed351bbf1bb0d2b747cb9cd6518626fc1649de
Filip M <filipm93@gmail.com>
2023-10-23 20:36:23 +0200

    add filtering for favorites and history

--- src/pages/account-page/account-page.tsx
+++ src/pages/account-page/account-page.tsx
@@ -1,14 +1,17 @@
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import { Box } from '@mui/system';
-import { useState, SyntheticEvent, useEffect } from 'react';
+import { useState, SyntheticEvent, useMemo, ChangeEvent } from 'react';
 import { TabPanel } from '../../components/tab-panel/tab-panel';
 import { useRecipes } from '../../contexts/recipes/recipes.context';
 import RecipeThumbnail from '../../components/recipe-thumbnail/recipe-thumbnail';
 import { useRecipeInfo } from '../../contexts/recipe-info/recipe-info-context';
 import { IRecipeData } from '../../services/recipe/recipe-data.interface';
+import { Input } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function AccountPage() {
+  const [searchQuery, setSearchQuery] = useState<string>('');
   const [activeTab, setActiveTab] = useState(0);
   const { allRecipes } = useRecipes();
   const [recipes] = useState<IRecipeData[]>(() => {
@@ -21,13 +24,34 @@ export default function AccountPage() {
       return prev;
     }, []);
   });
+
   const { isFavorite } = useRecipeInfo();
   const [favorites] = useState<IRecipeData[]>(() => {
     return recipes.filter((r) => isFavorite(r.id));
   });
 
+  const [filteredFavorites, setFilteredFavorites] = useState<IRecipeData[]>([]);
+  const [filteredHistory, setFilteredHistory] = useState<IRecipeData[]>([]);
+
+  useMemo(() => {
+    setFilteredFavorites(filterRecipes(favorites));
+    setFilteredHistory(filterRecipes(recipes));
+  }, [searchQuery]);
+
+  function filterRecipes(recipes: IRecipeData[]): IRecipeData[] {
+    return searchQuery.length > 0
+      ? recipes.filter((recipe) =>
+          recipe.title.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : recipes;
+  }
+
   const handleTabChange = (event: SyntheticEvent, newValue: number) => {
     setActiveTab(newValue);
+  };
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
   };
 
   return (
@@ -39,10 +63,26 @@ export default function AccountPage() {
         </Tabs>
       </Box>
       <TabPanel value={activeTab} index={0}>
-        {favorites?.map((r) => <RecipeThumbnail key={r.id} recipe={r} />)}
+        <Input
+          onChange={handleSearch}
+          value={searchQuery}
+          placeholder='Search for a recipe...'
+          endAdornment={<SearchIcon />}
+          sx={{ mb: 4 }}
+        ></Input>
+        {filteredFavorites?.map((r) => (
+          <RecipeThumbnail key={r.id} recipe={r} />
+        ))}
       </TabPanel>
       <TabPanel value={activeTab} index={1}>
-        {recipes.toReversed().map((r) => (
+        <Input
+          onChange={handleSearch}
+          value={searchQuery}
+          placeholder='Search for a recipe...'
+          endAdornment={<SearchIcon />}
+          sx={{ mb: 4 }}
+        ></Input>
+        {filteredHistory.toReversed().map((r) => (
           <RecipeThumbnail key={r.id} recipe={r} />
         ))}
       </TabPanel>

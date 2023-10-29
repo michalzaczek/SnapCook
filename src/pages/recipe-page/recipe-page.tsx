4660b2bc348f0e333ee3278edc85e74cedf78b4e
Filip M <filipm93@gmail.com>
2023-10-29 13:13:10 +0100

    recipe page mobile styles

--- src/pages/recipe-page/recipe-page.tsx
+++ src/pages/recipe-page/recipe-page.tsx
@@ -1,16 +1,37 @@
 import { NavLink, useParams } from 'react-router-dom';
 import { useRecipeInfo } from '../../contexts/recipe-info/recipe-info-context';
-import { Box, Grid, IconButton, Paper, Typography } from '@mui/material';
+import {
+  Box,
+  CardMedia,
+  Container,
+  Grid,
+  IconButton,
+  SxProps,
+  Typography,
+} from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import { useEffect, useState } from 'react';
 import { IRecipeInfo } from '../../contexts/recipe-info/recipe-info.interface';
+import TimerOutlinedIcon from '@mui/icons-material/TimerOutlined';
 
 export default function RecipePage() {
   const { id } = useParams();
   const { getRecipeInfo, setIsFavorite } = useRecipeInfo();
   const [recipe, setRecipe] = useState<IRecipeInfo>();
+
+  const iconStyle: SxProps = {
+    fontSize: '32px',
+  };
+
+  const h2Style: SxProps = {
+    textAlign: 'left',
+    fontSize: '24px',
+    fontFamily: 'Lato',
+    fontWeight: 700,
+    mb: 3,
+  };
 
   useEffect(() => {
     const getRecipe = async () => {
@@ -28,86 +49,140 @@ export default function RecipePage() {
   }
 
   return (
-    <>
-      <Grid
-        container
-        spacing={1}
-        width={'100vw'}
-        justifyContent={'center'}
-        alignContent={'center'}
-      >
-        <Grid item xs={12}>
-          <Paper
+    <Container sx={{ px: { xs: 0 } }}>
+      <Box mb={3}>
+        <CardMedia
+          sx={{
+            position: 'relative',
+            backgroundColor: 'grey.800',
+            color: '#fff',
+            mb: 0,
+            height: { xs: '300px' },
+          }}
+          image={recipe?.image}
+        >
+          <Box
             sx={{
-              position: 'relative',
-              height: '200px',
-              backgroundColor: 'grey.800',
-              color: '#fff',
-              mb: 0,
-              backgroundSize: 'cover',
-              backgroundRepeat: 'no-repeat',
-              backgroundPosition: 'center',
-              backgroundImage: `url(${recipe?.image})`,
+              backgroundColor: 'black',
+              position: 'absolute',
+              left: '0',
+              top: '0',
+              width: '100%',
+              height: '100px',
+              background:
+                'linear-gradient(180deg, rgba(0,0,0,0.7456232492997199) 0%, rgba(0,0,0,0.4907212885154062) 50%, rgba(0,212,255,0) 100%)',
+              display: { sm: 'none' },
+            }}
+          />
+          <Grid container>
+            <Grid item xs={6}>
+              <Box
+                sx={{
+                  position: 'relative',
+                  p: { xs: 3, md: 6 },
+                  pr: { md: 0 },
+                  textAlign: 'left',
+                }}
+              >
+                <IconButton component={NavLink} to={-1 as any}>
+                  <ArrowBackIcon
+                    sx={{
+                      color: 'primary.contrastText',
+                      ...iconStyle,
+                      position: 'relative',
+                      top: '-7px',
+                    }}
+                  />
+                </IconButton>
+              </Box>
+            </Grid>
+            <Grid item xs={6}>
+              <Box
+                sx={{
+                  position: 'relative',
+                  p: { xs: 3, md: 6 },
+                  pl: { md: 0 },
+                  textAlign: 'right',
+                }}
+              >
+                {recipe?.isFavorite ? (
+                  <FavoriteIcon
+                    onClick={() => setFavorite(recipe.id, false)}
+                    sx={iconStyle}
+                  />
+                ) : recipe ? (
+                  <FavoriteBorderOutlinedIcon
+                    onClick={() => setFavorite(recipe.id, true)}
+                    sx={iconStyle}
+                  />
+                ) : null}
+              </Box>
+            </Grid>
+          </Grid>
+        </CardMedia>
+      </Box>
+      <Box px={2} display='flex' flexDirection='column'>
+        <Box sx={{ borderBottom: '1px solid', pb: 2, mb: 2 }}>
+          <Typography
+            variant='h1'
+            sx={{
+              textAlign: 'left',
+              fontSize: '28px',
+              fontFamily: 'Lato',
+              fontWeight: 700,
             }}
           >
-            <Grid container>
-              <Grid item xs={6}>
-                <Box
-                  sx={{
-                    position: 'relative',
-                    p: { xs: 3, md: 6 },
-                    pr: { md: 0 },
-                    textAlign: 'left',
-                  }}
-                >
-                  <IconButton component={NavLink} to={-1 as any}>
-                    <ArrowBackIcon sx={{ color: 'primary.contrastText' }} />
-                  </IconButton>
-                </Box>
-              </Grid>
-              <Grid item xs={6}>
-                <Box
-                  sx={{
-                    position: 'relative',
-                    p: { xs: 3, md: 6 },
-                    pl: { md: 0 },
-                    textAlign: 'right',
-                  }}
-                >
-                  {recipe?.isFavorite ? (
-                    <FavoriteIcon
-                      onClick={() => setFavorite(recipe.id, false)}
-                    />
-                  ) : recipe ? (
-                    <FavoriteBorderOutlinedIcon
-                      onClick={() => setFavorite(recipe.id, true)}
-                    />
-                  ) : null}
-                </Box>
-              </Grid>
-            </Grid>
-          </Paper>
-        </Grid>
-        <Grid item xs={8}>
-          <Typography variant='h5'>{recipe?.title}</Typography>
-        </Grid>
-        <Grid item xs={8}>
-          <Typography variant='h6'>{recipe?.readyInMinutes} minutes</Typography>
-        </Grid>
-        <Grid item xs={8}>
+            {recipe?.title}
+          </Typography>
+        </Box>
+        <Box mb={3}>
+          <Typography
+            variant='h6'
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              fontSize: '18px',
+            }}
+          >
+            <TimerOutlinedIcon sx={{ mr: 1 }} />
+            {recipe?.readyInMinutes} minutes
+          </Typography>
+        </Box>
+        <Box mb={3}>
+          <Typography variant='h2' sx={h2Style}>
+            Ingredients
+          </Typography>
           {recipe?.extendedIngredients.map((ingredient, index) => {
             return (
-              <Paper key={index}>
-                <p>{ingredient.originalName || ingredient.originalString}</p>
-              </Paper>
+              <Box
+                key={index}
+                sx={{
+                  textAlign: 'left',
+                  borderBottom: '1px dotted',
+                  mb: 2,
+                  pb: 0.5,
+                }}
+              >
+                <span>
+                  <b>
+                    {ingredient.amount} {ingredient.unit}{' '}
+                  </b>
+                </span>
+                <span>
+                  {ingredient.originalName || ingredient.originalString}
+                </span>
+              </Box>
             );
           })}
-        </Grid>
-        <Grid item xs={8}>
-          <Typography variant='h6'>Steps</Typography>
+        </Box>
+        <Box textAlign='left'>
+          <Typography variant='h2' sx={h2Style}>
+            Instructions
+          </Typography>
           <p>{recipe?.instructions}</p>
-        </Grid>
-      </Grid>
-    </>
+        </Box>
+      </Box>
+    </Container>
   );
 }

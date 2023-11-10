d3470b0549283e4535306168bd3fa6117a3f8711
michalzaczek <michal.zaczek@interia.pl>
2023-11-10 14:14:15 +0100

    Refactor useStripePayment.ts, delete stripePayment.ts

--- src/hooks/useStripePayment.ts
+++ src/hooks/useStripePayment.ts
@@ -2,13 +2,11 @@ import { useContext, useState } from 'react';
 import { AuthContext } from '../contexts/auth/AuthContext';
 import { doc, collection, setDoc, onSnapshot } from 'firebase/firestore';
 import { db } from '../firebase/config';
-import { useNavigate } from 'react-router-dom';
 
 export const useStripePayment = () => {
   const { state } = useContext(AuthContext);
   const currentUser = state.user;
   const [error, setError] = useState<Error | null>(null);
-  const navigate = useNavigate();
 
   const redirectToCheckout = async (priceId: string) => {
     if (!currentUser || !currentUser.uid) {
@@ -17,7 +15,7 @@ export const useStripePayment = () => {
     }
 
     const checkoutSessionRef = doc(
-      collection(db, 'customers', currentUser.uid, 'checkout_sessions')
+      collection(db, 'users', currentUser.uid, 'checkout_sessions')
     );
 
     setDoc(checkoutSessionRef, {
@@ -25,7 +23,6 @@ export const useStripePayment = () => {
       success_url: window.location.origin,
       cancel_url: window.location.origin,
     }).catch((err) => {
-      console.error('Error creating checkout session:', err);
       setError(new Error('Failed to create checkout session'));
     });
 
@@ -33,19 +30,15 @@ export const useStripePayment = () => {
       checkoutSessionRef,
       (snap) => {
         const data = snap.data();
-        console.log('Snapshot data:', data); // Debugging line
         if (data?.error) {
-          console.error('Error from snapshot:', data.error); // Debugging line
           unsubscribe();
           setError(new Error(`An error occurred: ${data.error.message}`));
         } else if (data?.url) {
-          console.log('Redirecting to:', data.url); // Debugging line
           unsubscribe();
-          navigate(data.url);
+          window.location.href = data.url; // Redirect to Stripe
         }
       },
       (err) => {
-        console.error('Error listening to checkout session:', err); // Debugging line
         unsubscribe();
         setError(new Error('An error occurred while listening to the session'));
       }

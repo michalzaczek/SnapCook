8495ab6752657dd630bf5f42dfcdc29bbd38fc1a
Filip M <filipm93@gmail.com>
2023-11-15 22:53:41 +0100

    image upload - connect to snapcook api

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -6,6 +6,7 @@ import { useIngredients } from '../../contexts/ingredients/ingredients.context';
 import { CircularProgress, Input, Typography } from '@mui/material';
 import MainLayout from '../../components/main-layout/main-layout';
 import { useUIMessage } from '../../contexts/ui-message/ui-message.context';
+import { useAuth } from '../../contexts/auth/AuthContext';
 
 export default function HomePage() {
   const navigate = useNavigate();
@@ -13,22 +14,50 @@ export default function HomePage() {
   const [isLoading, setIsLoading] = useState(false);
   const { addIngredient, resetIngredients } = useIngredients();
   const { setMessage, setSeverity, setOpen } = useUIMessage();
+  const authState = useAuth().state;
 
   async function handleUpload(e: ChangeEvent<HTMLInputElement>): Promise<void> {
-    setIsLoading(true);
-
-    try {
-      const ingredients = (await fetchIngredients()).data || [];
-      resetIngredients();
-      ingredients.forEach((i) => addIngredient(i));
-      navigate('ingredients');
-    } catch (err: any) {
-      setSeverity('error');
-      setMessage('Failed to fetch the ingredients.');
-      setOpen(true);
+    if (!e.target.files || !e.target.files[0]) {
+      return;
     }
 
-    setIsLoading(false);
+    const reader = new FileReader();
+
+    reader.onload = async (event) => {
+      if (!event.target) {
+        return;
+      }
+
+      const userToken = await authState.user?.getIdToken();
+
+      if (!userToken) {
+        setSeverity('error');
+        setMessage('You need to sign in to use this feature.');
+        setOpen(true);
+        return;
+      }
+
+      const image = event.target.result as string;
+      const base64 = image.split(',')[1];
+
+      setIsLoading(true);
+
+      try {
+        const ingredients =
+          (await fetchIngredients(userToken, base64)).data || [];
+        resetIngredients();
+        ingredients.forEach((i) => addIngredient(i));
+        navigate('ingredients');
+      } catch (err: any) {
+        setSeverity('error');
+        setMessage('Failed to fetch the ingredients.');
+        setOpen(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
   }
 
   function handleClick() {
@@ -43,18 +72,18 @@ export default function HomePage() {
           style={{ height: 'auto', width: '250px' }}
           onClick={handleClick}
           src={snapCookLogo}
-          alt=""
+          alt=''
         />
       )}
       <Input
         inputRef={inputRef}
-        type="file"
+        type='file'
         inputProps={{ accept: 'image/*', capture: 'environment' }}
         onChange={handleUpload}
         sx={{ display: 'none' }}
       />
       <Typography
-        variant="subtitle1"
+        variant='subtitle1'
         sx={{ mt: 4, fontSize: '25px', color: 'primary.text' }}
       >
         Tap to take a photo...
--- src/services/ingredients/ingredients.service.ts
+++ src/services/ingredients/ingredients.service.ts
@@ -1,13 +1,20 @@
 import axios, { AxiosResponse } from 'axios';
 import { IIngredient } from '../../pages/ingredients-page/ingredient.interface';
 
-export const fetchIngredients = async (): Promise<
-  AxiosResponse<IIngredient[], any>
-> => {
+export const fetchIngredients = async (
+  userToken: string,
+  base64: string
+): Promise<AxiosResponse<IIngredient[], any>> => {
   try {
-    const response = await axios.request<IIngredient[]>({
-      url: 'http://localhost:3000/ingredients',
-    });
+    const response = await axios.post<IIngredient[]>(
+      'https://us-central1-snapcook-test.cloudfunctions.net/getIngredients',
+      { image: base64 },
+      {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      }
+    );
     return new Promise((resolve) => resolve(response));
   } catch (error: any) {
     throw error;
